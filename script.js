712e2f8ba524d38c37c3c1a0c07890e73190b17c
sssolano <ssolanocr132@gmail.com>
2026-05-08 21:16:20 -0600

    Add and delete button working locally

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-inputEl = document.getElementById("input-field")
-addBtn = document.getElementById("add-btn")
-ulEl = document.getElementById("ul-el")
+let inputEl = document.getElementById("input-field")
+const addBtn = document.getElementById("add-btn")
+let ulEl = document.getElementById("ul-el")
+const deleteAllBtn = document.getElementById("delete-all-btn")
+const updateBtn = document.getElementById("update-btn")
 
 let myTodo = []
+const myProto = {
+    "id": "text"
+}
+
+// Varias cosas por definir que otra funcion vamos hacer
+// Si habilitar poder editar la tarea
+// Editar el ESTADO de la tarea
+
 
 addBtn.addEventListener("click", function() {
     myTodo.push(inputEl.value)
@@ -11,9 +21,21 @@ addBtn.addEventListener("click", function() {
 })
 
 function show() {
+    // lo tenemos que tener vacio para poder agregar una nueva linea por cada loop que haga dentro del array
     let myList = ''
     for (let i = 0; i < myTodo.length; i++) {
-        myList += myTodo[i]
+        myList = `<li>${myTodo[i]}</li>`
     }
-    ulEl.innerHTML += `<li>${myList}</li>` 
+    // aqui hacemos que agregamos cada valor nuevo 
+    ulEl.innerHTML += myList
 }
+
+deleteAllBtn.addEventListener("click", function() {
+    myTodo = ""
+    ulEl.innerHTML = ""
+})
+
+updateBtn.addEventListener("click", function() {
+
+})
+
