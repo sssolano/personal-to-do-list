07450287236ccc80b3a8207654fbe384f53ca09f
sssolano <ssolanocr132@gmail.com>
2026-05-09 19:49:47 -0600

    Working for making objects instead of arrays

--- script.js
+++ script.js
@@ -4,18 +4,10 @@ let ulEl = document.getElementById("ul-el")
 const deleteAllBtn = document.getElementById("delete-all-btn")
 
 let myTodo = []
-// need to check if this is correct before doing this
-const myProto = {
-    "Tarea 1": true,
-}
-
-// Varias cosas por definir que otra funcion vamos hacer
-// Si habilitar poder editar la tarea
-// Editar el ESTADO de la tarea
-
 
 addBtn.addEventListener("click", function() {
-    myTodo.push(inputEl.value, false)
+    myTodo.push({"taskDescription": inputEl.value, "taskFinish": false})
+    console.log(myTodo)
     inputEl.value = ""
     show()
 })
@@ -24,7 +16,7 @@ function show() {
     // lo tenemos que tener vacio para poder agregar una nueva linea por cada loop que haga dentro del array
     let myList = ''
     for (let i = 0; i < myTodo.length; i++) {
-        myList += `<li><input type="checkbox"> ${myTodo[i]}</li>`
+        myList += `<li><input type="checkbox"> ${myTodo[i].taskDescription}</li>`
     }
     // aqui hacemos que agregamos cada valor nuevo 
     ulEl.innerHTML = myList
