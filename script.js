ce1db0b4de75a7a68cbfdb746bfbd27beba1bc8c
sssolano <ssolanocr132@gmail.com>
2026-05-12 18:51:55 -0600

    Render message of tasks completed and also checkmark stays

--- script.js
+++ script.js
@@ -15,18 +15,25 @@ addBtn.addEventListener("click", function() {
 function show() {
     // lo tenemos que tener vacio para poder agregar una nueva linea por cada loop que haga dentro del array
     let myList = ''
+    let counter = 0
     for (let i = 0; i < myTodo.length; i++) { 
-        myList += myTodo[i].taskFinish === true ? `<li class="cross-li"><input type="checkbox"> ${myTodo[i].taskDescription}</li>` :`<li><input type="checkbox"> ${myTodo[i].taskDescription}</li>`
+        if (myTodo[i].taskFinish === true) {
+            myList += `<li><input type="checkbox" checked> <span class="cross-li">${myTodo[i].taskDescription}</span> </li>`
+            counter++
+        } else {
+            myList += `<li><input type="checkbox"> <span>${myTodo[i].taskDescription}</span></li>`
+        }
     }
     // aqui hacemos que agregamos cada valor nuevo 
     ulEl.innerHTML = myList
     let checkBoxEl = ulEl.querySelectorAll('input[type="checkbox"]')
     for (let i = 0; i < checkBoxEl.length; i++) {
         checkBoxEl[i].addEventListener("click", function() {    
-            checkBoxEl[i].parentElement.classList.toggle("cross-li")
+            checkBoxEl[i].parentElement.querySelector('span').classList.toggle("cross-li")
             myTodo[i].taskFinish = !myTodo[i].taskFinish 
         }) 
     }
+    renderEl.innerHTML = `<p>${counter} of ${myTodo.length} completed</p>`
 }
 
 deleteAllBtn.addEventListener("click", function() {
